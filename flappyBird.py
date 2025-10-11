7be18fc99769849679a5a5840b0de15bbb60a1d2
omkarmacharyaCPU <karmacharyaom9@gmail.com>
2025-10-11 15:57:15 +0545

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -1,180 +1,190 @@
 import pygame
-import random 
+import random
 
-pygame.init()
+pygame.init() 
 
-clock = pygame.time.Clock()
-MAX_FPS = 60
-SCROLL_SPEED = 5
 score = 0
+nextChangeInDistance = 100
+font = pygame.font.SysFont (None, 60)
+isAlive = True 
 gameHasStarted = False
 
-minecraftFont = pygame.font.Font (r"fonts/Minecraft.ttf", 25)
-flappyBirdFontRegular = pygame.font.Font (r"fonts/flappybirdyregularfonty.ttf", 50)
+CLOCK = pygame.time.Clock()
+MAX_FPS = 60
+SCROLL_SPEED = 5   
 
-def writeText (text, textColor, font, x, y):
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 750
+screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption ("Flappy Birdy!")
+icon = pygame.image.load (r"images/icon.png")
+pygame.display.set_icon (icon)
+
+# Functions:
+def printText (text, font, textColor, x, y):
     textImg = font.render (text, True, textColor)
     screen.blit (textImg, (x, y))
 
 def death():
-    global BIRD_ACCELERATION, canFly, isAlive, birdVelocity, SCROLL_SPEED
+    global SCROLL_SPEED, BIRD_ACCELERATION, birdVelocity, isAlive
+    SCROLL_SPEED = 0
     BIRD_ACCELERATION = 0
     birdVelocity = 0
-    canFly = False 
     isAlive = False 
-    SCROLL_SPEED = 0
-    writeText ("You Died!", (0, 0, 0), flappyBirdFontRegular, 180, 300)
-    writeText ("Press Enter To Start Again!", (0, 0, 0), flappyBirdFontRegular, 30, 350)
+    printText ("Press Enter to Restart!", font, (0, 0, 0), 25, 300)
 
 def reset():
-    global birdPositionX, gameHasStarted, birdPositionY, BIRD_ACCELERATION, canFly, isAlive, birdVelocity, SCROLL_SPEED, score 
-    BIRD_ACCELERATION = 0.5
-    canFly = True 
-    isAlive = True 
-    birdVelocity = 0
+    global SCROLL_SPEED, BIRD_ACCELERATION, birdVelocity, isAlive, pipes, gameHasStarted, score, birdPositionX, birdPositionY
     SCROLL_SPEED = 5
-    pipes.clear()
-    score = 0
+    BIRD_ACCELERATION = 0.5
+    birdVelocity = 0
+    isAlive = True 
+    pipes.clear ()
     birdPositionX = 100
-    birdPositionY = 300
+    birdPositionY = 250
+    score = 0
     gameHasStarted = False
 
-# Screen
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 750
-screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# Background
+# Background:
 BG_WIDTH = 500
-BG_HEIGHT = 550
+BG_HEIGHT = 600
 bg = pygame.image.load (r"images/bg.png")
 bg = pygame.transform.scale (bg, (BG_WIDTH, BG_HEIGHT))
 
-# Ground
-ground1 = pygame.image.load (r"images/ground.png")
-gpx1 = 0
-GROUND_WIDTH = ground1.get_width()
-GROUND_HEIGHT = ground1.get_height()
-ground2 = pygame.image.load (r"images/ground.png")
-gpx2 = GROUND_WIDTH 
+# Ground:
+ground = pygame.image.load (r"images/ground.png")
+GROUND_WIDTH = ground.get_width()
+GROUND_HEIGHT = ground.get_height()
 
-# Bird
-birdFlapUp = pygame.image.load (r"images/flapUp.png")
-birdFlapMid = pygame.image.load (r"images/flapMid.png")
-birdFlapDown = pygame.image.load (r"images/flapDown.png")
-birdDead = pygame.image.load (r"images/deadBird.png")
-bird = birdFlapMid 
+ground1 = ground
+groundPositionX1 = 0
+ground2 = ground
+groundPositionX2 = GROUND_WIDTH
+
+# Bird:
+flapUp = pygame.image.load (r"images/flapUp.png")
+flapDown = pygame.image.load (r"images/flapDown.png")
+flapMid = pygame.image.load (r"images/flapMid.png")
+bird = flapMid
+birdDead = pygame.image.load (r"images/birdDead.png")
 BIRD_WIDTH = bird.get_width()
 BIRD_HEIGHT = bird.get_height()
-BIRD_ACCELERATION = 0.5 # pixels / frame ^ 2
-birdVelocity = 0 # pixels / frame
-birdPE = -8 
 changeBirdFrameIndex = 0
-birdPositionX = 100 
-birdPositionY = 300 
-canFly = True
-isAlive = True 
+birdPositionX = 100
+birdPositionY = 250
+birdVelocity = 0
+BIRD_ACCELERATION = 0.5
+BIRD_PE = 8
 
-# Pipe
+# Pipes:
 pipes = []
 pipeBottom = pygame.image.load (r"images/pipeBottom.png")
 pipeTop = pygame.transform.flip (pipeBottom, False, True)
-pipeSpawnCoolDown = 60
-PIPE_DISTANCE = 200 
+PIPE_DISTANCE = 200
+pipeCooldown = 0 # 60
 PIPE_WIDTH = pipeBottom.get_width()
 PIPE_HEIGHT = pipeBottom.get_height()
 
-isRunning = True 
-while isRunning:
+isRunning = True
+while (isRunning):
+
+    CLOCK.tick (MAX_FPS)
+
+    screen.blit (bg, (0, 0))
+    screen.blit (ground1, (groundPositionX1, BG_HEIGHT))
+    screen.blit (ground2, (groundPositionX2, BG_HEIGHT))
+
+    groundPositionX1 -= SCROLL_SPEED
+    groundPositionX2 -= SCROLL_SPEED
+    if (groundPositionX1 <= -GROUND_WIDTH):
+        groundPositionX1 = GROUND_WIDTH
+    if (groundPositionX2 <= -GROUND_WIDTH):
+        groundPositionX2 = GROUND_WIDTH
+
+    birdRect = pygame.Rect (birdPositionX, birdPositionY, BIRD_WIDTH, BIRD_HEIGHT)
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
-            isRunning = False
+            isRunning = False 
         if gameHasStarted:
             if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_SPACE and canFly:
-                    birdVelocity = birdPE
+                if isAlive and e.key == pygame.K_SPACE:
+                    birdVelocity = -BIRD_PE
                 if not isAlive:
                     if e.key == pygame.K_RETURN:
-                        reset() 
-        if not gameHasStarted:
-            screen.blit (bg, (0, 0))
-            screen.blit (ground1, (gpx1, BG_HEIGHT))
-            writeText ("Press Space To Start", (0, 0, 0), minecraftFont, 100, 300)
+                        reset()
+        else:
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_SPACE:
-                        gameHasStarted = True
-            pygame.display.update()
+                    gameHasStarted = True 
+                    birdVelocity = -BIRD_PE
+
+    if (isAlive):
+        changeBirdFrameIndex += 1
+        if (changeBirdFrameIndex < 5):
+            bird = flapUp
+        elif (changeBirdFrameIndex < 10):
+            bird = flapMid 
+        elif (changeBirdFrameIndex < 15):
+            bird = flapDown 
+        elif (changeBirdFrameIndex < 20):
+            bird = flapMid
+            if (changeBirdFrameIndex == 19):
+                changeBirdFrameIndex = 0
+    else:
+        bird = birdDead
 
     if gameHasStarted:
-        clock.tick (MAX_FPS)
-        screen.blit (bg, (0, 0))
-
-        screen.blit (ground1, (gpx1, BG_HEIGHT))
-        screen.blit (ground2, (gpx2, BG_HEIGHT))
-        
-        gpx1 -= SCROLL_SPEED
-        gpx2 -= SCROLL_SPEED
-        if (gpx1 <= - GROUND_WIDTH):
-            gpx1 = GROUND_WIDTH
-        if (gpx2 <= - GROUND_WIDTH):
-            gpx2 = GROUND_WIDTH
-
-        if isAlive:
-            changeBirdFrameIndex += 1
-            if (changeBirdFrameIndex < 5):
-                bird = birdFlapUp
-            elif (changeBirdFrameIndex < 10):
-                bird = birdFlapMid
-            elif (changeBirdFrameIndex < 15):
-                bird = birdFlapDown
-            elif (changeBirdFrameIndex <= 20):
-                bird = birdFlapMid
-                if changeBirdFrameIndex == 20:
-                    changeBirdFrameIndex = 0
-        else:
-            bird = birdDead
-
-        birdVelocity += BIRD_ACCELERATION
         birdPositionY += birdVelocity
-        
-        angle = birdVelocity * -3
+        birdVelocity += BIRD_ACCELERATION
+
+        angle = birdVelocity * -4
         rotatedBird = pygame.transform.rotate (bird, angle)
 
-        birdRect = pygame.Rect (birdPositionX, birdPositionY, BIRD_WIDTH, BIRD_HEIGHT)
-
-        if (pipeSpawnCoolDown == 0):
-            x = 500
-            y = random.randint (300, 550)
-            pipes.append ([x, y, False]) # x, y, scored? 
-            pipeSpawnCoolDown = 60
+        if (pipeCooldown == 0):
+            x = SCREEN_WIDTH
+            y = random.randint (225, 575)
+            pipes.append ([x, y, False]) # x, y, scored?
+            pipeCooldown = 60
         else:
-            pipeSpawnCoolDown -= 1
+            pipeCooldown -= 1
 
-        for p in pipes [:]:
+        for p in pipes[:]:
             if p[0] <= -PIPE_WIDTH:
                 pipes.remove (p)
+                continue 
             p[0] -= SCROLL_SPEED
             screen.blit (pipeBottom, (p[0], p[1]))
             screen.blit (pipeTop, (p[0], p[1] - PIPE_DISTANCE - PIPE_HEIGHT))
 
-            if birdPositionX > p[0] and not p[2]:
-                score += 1
-                p[2] = True 
-            
-            pipeBottomRect = pygame.Rect (p[0], p[1], PIPE_WIDTH, PIPE_HEIGHT)
-            pipeTopRect = pygame.Rect (p[0], p[1] - PIPE_HEIGHT - PIPE_DISTANCE, PIPE_WIDTH, PIPE_HEIGHT)
+            if not p[2]:
+                if birdPositionX > p[0] + PIPE_WIDTH // 2:
+                    score += 1
+                    p[2] = True
 
-            if (birdRect.colliderect (pipeBottomRect) or birdRect.colliderect (pipeTopRect)):
+            pipeBottomRect = pygame.Rect (p[0], p[1], PIPE_WIDTH, PIPE_HEIGHT)
+            pipeTopRect = pygame.Rect (p[0], p[1] - PIPE_DISTANCE - PIPE_HEIGHT, PIPE_WIDTH, PIPE_HEIGHT)
+
+            if (pipeBottomRect.colliderect (birdRect) or pipeTopRect.colliderect (birdRect)):
                 death()
 
-        screen.blit (rotatedBird, (birdPositionX, birdPositionY))
-
-        if (birdPositionY + BIRD_HEIGHT >= BG_HEIGHT):
+        if (birdPositionY >= BG_HEIGHT - BIRD_HEIGHT):
+            death()
+        elif (birdPositionY <= 0):
             death()
 
-        writeText (f"{score}", (0, 0, 0), minecraftFont, 225, 100)
+        screen.blit (rotatedBird, (birdPositionX, birdPositionY))
+        printText (f"{score}", font, (0, 0, 0), SCREEN_WIDTH // 2 - 5, SCREEN_HEIGHT // 2 - 300)
 
-        pygame.display.update()
+        if (score >= nextChangeInDistance):
+            PIPE_DISTANCE -= 5
+            nextChangeInDistance += 100
+            
+
+    else:
+        printText ("Press Space To Start!", font, (0, 0, 0), 35, 300)
+        screen.blit (bird, (birdPositionX, birdPositionY))
+
+    pygame.display.flip()
 
 pygame.quit()
